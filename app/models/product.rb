a8f93d9b250061093952bd6a8527a6845034ebf7
ONIKIE <whiteriot0024@gmail.com>
2022-08-14 21:18:38 +0900

    バリデーションの記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,16 @@ class Product < ApplicationRecord
   has_one_attached :image
   belongs_to :user
  
+
+  validates :image,           presence: true
+  validates :name,            presence: true
+  validates :explanation,     presence: true
+  validates :price,           presence: true
+  validates :category_id,     presence: true
+  validates :status_id,       presence: true
+  validates :shipping_fee_id, presence: true
+  validates :prefecture_id,   presence: true
+  validates :day_id,          presence: true
+  
+
 end
