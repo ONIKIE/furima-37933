53a96fe6d4cc8d329739cf9fe01c77ba58169ed4
ONIKIE <whiteriot0024@gmail.com>
2022-08-15 09:33:26 +0900

    ActiveHashの導入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,12 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.order('created_at DESC')
+  end
+
+  def new
+  end
+
+  def create
   end
 
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,13 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :day
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :status
   has_one_attached :image
+
+
   belongs_to :user
  
 
@@ -12,6 +20,14 @@ class Product < ApplicationRecord
   validates :shipping_fee_id, presence: true
   validates :prefecture_id,   presence: true
   validates :day_id,          presence: true
-  
 
+  with_options numericality: {other_than 1, message: 'select'} do
+    validates :category_id
+    validates :day_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :status_id
+  end
+
+validates :price, format: { with: /\A[0-9]+\z/, message: 'Price Half-width number' }, inclusion: { in: (300..9_999_999), message: 'Out of setting range' }
 end
