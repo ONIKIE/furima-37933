0a727e09f3a5dd00bf97fc577adc20ee6f65cc71
ONIKIE <whiteriot0024@gmail.com>
2022-08-20 18:29:17 +0900

    バリデーションの修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,15 +9,17 @@ class Product < ApplicationRecord
 
   belongs_to :user
 
-  validates :image,           presence: true
-  validates :name,            presence: true
-  validates :explanation,     presence: true
-  validates :price,           presence: true
-  validates :category_id,     presence: true
-  validates :status_id,       presence: true
-  validates :shipping_fee_id, presence: true
-  validates :prefecture_id,   presence: true
-  validates :day_id,          presence: true
+  with_options presence: true do
+  validates :image
+  validates :name
+  validates :explanation
+  validates :price
+  validates :category_id
+  validates :status_id
+  validates :shipping_fee_id
+  validates :prefecture_id
+  validates :day_id
+  end
 
   with_options numericality: { other_than: 1, message: 'select' } do
     validates :category_id
@@ -27,6 +29,5 @@ class Product < ApplicationRecord
     validates :status_id
   end
 
-  validates :price, format: { with: /\A[0-9]+\z/, message: 'Price Half-width number' },
-                    inclusion: { in: (300..9_999_999), message: 'Out of setting range' }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
