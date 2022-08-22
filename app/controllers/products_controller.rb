f2336deb6506503b966750bfb51bf7c605ff3611
ONIKIE <whiteriot0024@gmail.com>
2022-08-22 21:07:46 +0900

    showアクション定義・set_productメソッド作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_product, only: [:show]
 
   def index
      @products = Product.all.order('created_at DESC')
@@ -18,6 +19,11 @@ class ProductsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  
+
   private
 
   def require_login
@@ -27,5 +33,9 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:image, :name, :explanation, :category_id, :shipping_fee_id, :status_id, :prefecture_id,
                                     :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
