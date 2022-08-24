0c147a7efbc874f85bf5127f69d2f74d12de6c23
ONIKIE <whiteriot0024@gmail.com>
2022-08-24 23:16:38 +0900

    edit.updateアクション定義

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_product, only: [:show]
+  before_action :set_product, only: [:show, :edit, :update]
 
   def index
     @products = Product.all.order('created_at DESC')
@@ -22,12 +22,29 @@ class ProductsController < ApplicationController
   def show
   end
 
+  def edit
+    end
+
+  def update
+    if @product.update(product_params)
+      redirect_to product_path
+    else
+      render 'edit'
+    end
+  end
+
   private
 
 
   def product_params
     params.require(:product).permit(:image, :name, :explanation, :category_id, :shipping_fee_id, :status_id, :prefecture_id,
                                     :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def ensure_current_user
+    product = Product.find(params[:id])
+    if product.user_id != current_user.id
+    end
   end
 
   def set_product
