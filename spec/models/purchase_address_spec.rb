a3de48ce8bde46bcbeb7329231e8a1a32835aa4c
ONIKIE <whiteriot0024@gmail.com>
2022-09-03 21:26:06 +0900

    テストコード修正

--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -1,72 +1,104 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseAddress, type: :model do
-  describe '#create' do
+  describe '商品購入機能' do
     before do
       @user = FactoryBot.create(:user)
       @product = FactoryBot.build(:product)
       @product.image = fixture_file_upload('public/images/test_image.png')
       @product.save
       @purchase = FactoryBot.build(:purchase_address, product_id: @product.id, user_id: @user.id)
-      sleep 0.1
-    end
-    # 購入関連
-    it '必須項目を全て入力すれば購入ができる' do
-      expect(@purchase).to  be_valid
     end
 
-    it '郵便番号が空だと登録できない' do
-      @purchase.postal_code = nil
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include("Postal code can't be blank")
-    end
 
-    it '都道府県が「---」だと登録できない' do
-      @purchase.prefecture_id = 1
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include('Prefecture select')
-    end
+      context '内容に問題がない場合' do
+        it 'すべての値が正しく入力されていれば保存できる' do
+          expect(@purchase).to be_valid
+        end
 
-    it '都道府県が空だと保存できないこと' do
-      @purchase.prefecture_id = nil
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include("Prefecture can't be blank")
-    end
+        it 'user_idが空でなければ保存できる' do
+          @purchase.user_id = 1
+          expect(@purchase).to be_valid
+        end
 
-    it '市区町村が空だと登録できない' do
-      @purchase.municipality = nil
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include("Municipality can't be blank")
-    end
+        it 'product_idが空でなければ保存できる' do
+          @purchase.product_id = 1
+          expect(@purchase).to be_valid
+        end
 
-    it '番地が空だと登録できない' do
-      @purchase.address = nil
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include("Address can't be blank")
-    end
+        it '建物名が空でも保存できる' do
+          @purchase.building_name = nil
+          expect(@purchase).to be_valid
+        end
+      end
 
-    it '電話番号が空だと登録できない' do
-      @purchase.phone_number = nil
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include("Phone number can't be blank")
-    end
 
-    it '電話番号にハイフンがあると登録できない' do
-      @purchase.phone_number = '000-0000-0000'
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include('Phone number is invalid')
-    end
+      context '内容に問題がある場合' do
+        it 'user_idが空だと保存できない' do
+          @purchase.user_id = nil
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("User can't be blank")
+        end
 
-    it '電話番号が12桁以上あると保存できないこと' do
-      @purchase.phone_number = 12_345_678_910_123_111
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include('Phone number is invalid')
-    end
+        it 'product_idが空だと保存できない' do
+          @purchase.product_id = nil
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Product can't be blank")
+        end
 
-    it '郵便番号にハイフンがないと登録できない' do
-      @purchase.postal_code = '7777777'
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include('Postal code Input correctly')
+        it '郵便番号が空だと保存できない' do
+          @purchase.postal_code = nil
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Postal code can't be blank", "Postal code Input correctly")
+        end
+
+        it '郵便番号にハイフンがないと保存できない' do
+          @purchase.postal_code = 1234567
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Postal code Input correctly")
+        end
+
+        it '都道府県が「---」だと保存できない' do
+          @purchase.prefecture_id = 1
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Prefecture select")
+        end
+
+        it '都道府県が空だと保存できない' do
+          @purchase.prefecture_id = nil
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Prefecture can't be blank")
+        end
+
+        it '市区町村が空だと保存できない' do
+          @purchase.municipality = nil
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Municipality can't be blank")
+        end
+
+        it '番地が空だと保存できない' do
+          @purchase.address = nil
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Address can't be blank")
+        end
+
+        it '電話番号が空だと保存できない' do
+          @purchase.phone_number = nil
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Phone number can't be blank")
+        end
+
+        it '電話番号が12桁以上あると保存できない' do
+          @purchase.phone_number = 12_345_678_910_123_111
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include('Phone number is invalid')
+        end
+
+        it 'トークンが空だと保存できない' do
+          @purchase.token = nil
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Token can't be blank")
+        end
+      end
     end
   end
-end
