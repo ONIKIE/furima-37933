2618299b8686a5a7ffeb7ca802814a2a28558dae
ONIKIE <whiteriot0024@gmail.com>
2022-09-03 21:27:50 +0900

    factorybot追加・アソシエーション削除

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
   factory :purchase_address do
-    token { 'rhitkkkdioejk' }
+    user_id { Faker::Number.non_zero_digit }
+    product_id { Faker::Number.non_zero_digit }
     postal_code { '777-7777' }
-    prefecture_id { Faker::Number.within(range: 1..2) }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
     municipality { Gimei.address.city.kanji }
     address { Gimei.address.town.kanji }
     phone_number { '00000000000' }
-    association :product
-    association :user
+    building_name { Faker::Address.street_address }
+    token { Faker::Internet.password(min_length: 20, max_length: 30) }
   end
 end
