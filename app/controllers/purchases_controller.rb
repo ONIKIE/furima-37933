1205aa293c8b19ea343d7e42952eaf8c354d5574
ONIKIE <whiteriot0024@gmail.com>
2022-09-03 10:32:52 +0900

    クレジット決済機能

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,6 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_product, only: [:index, :create]
+  before_action :set_product, only: [:index, :create, :pay_item]
 
   def index
     if @product[:user_id] == current_user.id || @product.purchase.present?
@@ -11,14 +11,31 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    @purchase = PurchaseAddress.new(purchase_params) 
+    @purchase = PurchaseAddress.new(purchase_params)
+   
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      redirect_to root_path
+    else
+      render :index
+    end 
   end
 
 
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, product_id: params[:product_id],token: params[:token])
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :municipality, :building_name, :address, :phone_number).merge(user_id: current_user.id, product_id: params[:product_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 自身のPAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @product.price,
+      card: params[:token],
+      currency: 'jpy'
+    )
   end
 
   def set_product
---
+++ app/javascript/card.js
@@ -0,0 +1,41 @@
+const pay = () => {
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+  // charge_formの要素を取得し、変数に定義
+  const form = document.getElementById("charge-form");
+  // クリックで実行
+  form.addEventListener("submit", (e) => {
+  // 通常のRuby on Railsにおけるフォーム送信処理はキャンセルされる
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+   // formDataより取得したクレジットカード情報をcardに代入
+    const card = {
+      number: formData.get("purchase_address[number]"),
+      cvc: formData.get("purchase_address[cvc]"),
+      exp_month: formData.get("purchase_address[exp_month]"),
+      exp_year: `20${formData.get("purchase_address[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      console.log()
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+      // フォームに残っているクレジットカード情報削除
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+
+      // フォームの情報をサーバーサイドに送信
+      document.getElementById("charge-form").submit();
+      document.getElementById("charge-form").reset();
+    });
+  });
+};
+window.addEventListener("load", pay);
