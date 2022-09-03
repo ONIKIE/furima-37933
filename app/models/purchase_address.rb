d4a98bf44e3d4431f6f91d678d93db121f08ab23
ONIKIE <whiteriot0024@gmail.com>
2022-09-03 14:18:40 +0900

    バリデーションの追加

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -5,6 +5,8 @@ class PurchaseAddress
                 :token
 
   with_options presence: true do
+    validates :user_id
+    validates :item_id
     validates :token
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'Input correctly' }
     validates :prefecture_id, numericality: { other_than: 1, message: 'select' }
